fbad647c0f8a258238de9a3ccac485cc996479c7
Jason Chen <jasonchen6699@gmail.com>
2017-01-31 07:14:56 -0500

    Only execute block if run in the command line
    Add __name__ == "__main__" check

--- valsi.py
+++ valsi.py
@@ -7,8 +7,10 @@ parser = argparse.ArgumentParser(description="Grab words from vlasisku.")
 parser.add_argument("--type", required=True, choices=["gismu",
                     "cmavo", "experimental-cmavo", "fu'ivla", "cmene"], help="The type of word to grab.")
 
-word_type = parser.parse_args().type
+# Only execute the following if used in the command line.
+if __name__ == "__main__":
+    word_type = parser.parse_args().type
 
-# Words of type [word_type] can be found at the url below
-url = "http://vlasisku.lojban.org/vlasisku/" + word_type
-content = urlopen(url).read()
+    # Words of type [word_type] can be found at the url below
+    url = "http://vlasisku.lojban.org/vlasisku/" + word_type
+    content = urlopen(url).read()
